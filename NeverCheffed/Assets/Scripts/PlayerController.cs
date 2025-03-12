7cd573ee7ac608bc74a98e11e46189bca8200d55
Boogaloo808 <boogaloo808@icloud.com>
2025-03-12 09:44:38 -0700

    Beginning of NeverCheffed
    The beginning of the Game

---
+++ NeverCheffed/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    
+}
