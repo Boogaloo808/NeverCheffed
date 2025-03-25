8c14905517679fd861ee229a4c74001425fb7e82
Boogaloo808 <boogaloo808@icloud.com>
2025-03-25 10:24:03 -0700

    spawner

--- NeverCheffed/Assets/Scripts/Food/FallingFood.cs
+++ NeverCheffed/Assets/Scripts/Food/FallingFood.cs
@@ -11,6 +11,8 @@ public class FallingFood : MonoBehaviour
     bool wait;
     public Collider2D roomCollider;
 
+    public int foodCategory;
+
     bool playerInRoom = false;
     public bool spawned = false;
 
@@ -25,18 +27,45 @@ public class FallingFood : MonoBehaviour
         wait = true;
         StartCoroutine("SpawnTime", 0);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+            if (collision.gameObject.tag == "Player")
+            {
+                playerInRoom = true;
+            }
+    }
+
     void Update()
     {
-        
 
-        if ((player.activated) && (spawned == false) && !wait)
+        if ((player.activated) && (spawned == false) && !wait && (playerInRoom))
             Invoke("Spawn", 0f);
     } 
 
     IEnumerator SpawnTime(float seconds)
     {
-        rand = Random.Range(0, foodlist.ingredients.Length);
-        Instantiate(foodlist.ingredients[rand], transform.position + Vector3.right * Random.Range(-10, 10f), foodlist.ingredients[rand].transform.rotation);
+        if (foodCategory == 1)
+        {
+            rand = Random.Range(0, foodlist.fridgeRoom.Length);
+            Instantiate(foodlist.fridgeRoom[rand], transform.position + Vector3.right * Random.Range(-10, 10f), foodlist.fridgeRoom[rand].transform.rotation);
+        }
+        else if (foodCategory == 2)
+        {
+            rand = Random.Range(0, foodlist.freezerRoom.Length);
+            Instantiate(foodlist.freezerRoom[rand], transform.position + Vector3.right * Random.Range(-10, 10f), foodlist.freezerRoom[rand].transform.rotation);
+        }
+        else if (foodCategory == 3)
+        {
+            rand = Random.Range(0, foodlist.pantryRoom.Length);
+            Instantiate(foodlist.pantryRoom[rand], transform.position + Vector3.right * Random.Range(-10, 10f), foodlist.pantryRoom[rand].transform.rotation);
+        }
+        else if (foodCategory == 4)
+        {
+            rand = Random.Range(0, foodlist.freezerRoom.Length);
+            Instantiate(foodlist.freezerRoom[rand], transform.position + Vector3.right * Random.Range(-10, 10f), foodlist.spiceRoom[rand].transform.rotation);
+        }
+
         yield return new WaitForSeconds(0.6f);
         wait = false;
     }
--- NeverCheffed/Assets/Scripts/Food/FoodList.cs
+++ NeverCheffed/Assets/Scripts/Food/FoodList.cs
@@ -5,19 +5,36 @@ using UnityEngine.UI;
 
 public class FoodList : MonoBehaviour
 {
-    public GameObject[] ingredients;
+    [Header("Rooms")]
+    public GameObject[] fridgeRoom;
+    public GameObject[] freezerRoom;
+    public GameObject[] pantryRoom;
+    public GameObject[] spiceRoom;
+
+    [Header("Food")]
     public GameObject[] dishes;
 
-    public List<GameObject> rooms;
-
+    public List<GameObject> foodInPan;
 
     void Start()
     {
-        
+        foodInPan = new List<GameObject>();
     }
 
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "food")
+            foodInPan.Add(collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "food")
+            foodInPan.Remove(collision.gameObject);
     }
 }
