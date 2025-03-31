5bb531eaffac99d979f67959b3d643811282ef12
Boogaloo808 <boogaloo808@icloud.com>
2025-03-31 10:24:48 -0700

    Cooked food

--- NeverCheffed/Assets/Scripts/Food/PanScript.cs
+++ NeverCheffed/Assets/Scripts/Food/PanScript.cs
@@ -1,13 +1,20 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PanScript : MonoBehaviour
 {
     public List<GameObject> foodInPan;
+    public Transform FFT;
+
+    public Collider2D panCollider;
+
+    public GameManager manager;
 
     void Start()
     {
         foodInPan = new List<GameObject>();
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -17,7 +24,14 @@ public class PanScript : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Food")) foodInPan.Add(collision.gameObject);
+        else if (collision.gameObject.CompareTag("Food")) transform.SetParent(gameObject.transform, true);
     }
 
-
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Food") && manager.Cooked)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 }
