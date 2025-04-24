a1890159d513878ecc1f00bb9a999b0829f5c2af
Boogaloo808 <boogaloo808@icloud.com>
2025-04-24 10:19:13 -0700

    finsuih

--- NeverCheffed/Assets/Scripts/Food/FallingFood.cs
+++ NeverCheffed/Assets/Scripts/Food/FallingFood.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.WSA;
 
 public class FallingFood : MonoBehaviour
 {
--- NeverCheffed/Assets/Scripts/Ordering/OrderTicket.cs
+++ NeverCheffed/Assets/Scripts/Ordering/OrderTicket.cs
@@ -1,5 +1,4 @@
 using System.Collections.Concurrent;
-using UnityEditor.Build;
 using UnityEngine;
 
 public class OrderTicket : MonoBehaviour
